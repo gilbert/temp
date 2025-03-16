f1d608fd11698b396f02e1e955efafe7955ce145
Rasmus Porsager <rasmus@porsager.com>
2025-03-16 19:51:13 +0100

    Fix absolute internal links

--- src/index.js
+++ src/index.js
@@ -1001,7 +1001,7 @@ function attributes(dom, view, context) {
 
   if (hasOwn.call(view.attrs, 'href') && (context.hydrating || !prev || prev.href !== view.attrs.href)) {
     value = view.attrs.href
-    const internal = !String(value).match(/^[a-z]+:|\/\//) && !view.attrs.target && !view.attrs.download
+    const internal = !String(value).match(/^([a-z]+:|\/\/)/) && !view.attrs.target && !view.attrs.download
     internal && (value = cleanHref(view.attrs.href))
     updateAttribute(dom, view.attrs, 'href', prev && prev.href, value, create)
     if (value && internal) {
