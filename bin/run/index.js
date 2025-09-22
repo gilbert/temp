c8fef0478bdfdf0e0c7b33af0c0fbd02001df256
Rasmus Porsager <rasmus@porsager.com>
2025-09-21 22:43:24 -0600

    Make run serial and sync - fix signals and rely on child

--- bin/run/index.js
+++ bin/run/index.js
@@ -1,12 +1,12 @@
 import cp from 'node:child_process'
 import Path from 'node:path'
 import fs from 'node:fs'
-import prexit from '../prexit.js'
 
 import config from '../config.js'
 import c from '../color.js'
 
 const p = console.log // eslint-disable-line
+const noop = () => { /* noop - child handles */ }
 
 let pkg
 let dir = config.cwd
@@ -19,49 +19,48 @@ while (!(pkg = await jsonRead(Path.join(dir, 'package.json')))) {
 
 process.chdir(process.env.PWD = dir)
 
-config._.length === 0
-  ? print(pkg)
-  : await run(config._, pkg)
+if (config._.length === 0) {
+  print(pkg)
+} else {
+  for (const x of config._) {
+    const cmd = pkg.scripts[x]
+    if (!cmd)
+      throw new Error('No script found for: ' + x)
+  
+    const bins = [Path.join(config.globalDir, 'node_modules', '.bin')]
+    let dir = config.cwd
+    let prev
+    let error
+    while (dir !== prev) {
+      const binDir = Path.join(dir, 'node_modules', '.bin')
+      if (fs.existsSync(binDir))
+        bins.unshift(binDir)
+      prev = dir
+      dir = Path.dirname(dir)
+    }
+    
+    'SIGINT SIGTERM SIGHUP SIGBREAK'.split(' ').forEach(x => process.on(x, noop))
+    process.exitCode = cp.spawnSync(cmd, [], {
+      stdio: 'inherit',
+      shell: true,
+      env: {
+        ...process.env,
+        PATH: bins.join(':') + ':' + process.env.PATH
+      }
+    }).status
+    
+    if (process.exitCode !== 0)
+      break
+    
+    'SIGINT SIGTERM SIGHUP SIGBREAK'.split(' ').forEach(x => process.off(x, noop))
+  }
+}
 
 function print(pkg) {
   for (const [name, script] of Object.entries(pkg.scripts)) {
     p('  ' + name)
     p('    ' + c.dim(script))
   }
-}
-
-async function run(xs, pkg) {
-  await Promise.allSettled(xs.map(x => {
-    const promise = new Promise((resolve, reject) => {
-      const cmd = pkg.scripts[x]
-      if (!cmd)
-        return reject(new Error('No script found for: ' + x))
-
-      const bins = [Path.join(config.globalDir, 'node_modules', '.bin')]
-      let dir = config.cwd
-      let prev
-      while (dir !== prev) {
-        const binDir = Path.join(dir, 'node_modules', '.bin')
-        if (fs.existsSync(binDir))
-          bins.unshift(binDir)
-        prev = dir
-        dir = Path.dirname(dir)
-      }
-
-      const p = cp.spawn(cmd, [], {
-        shell: true,
-        stdio: 'inherit',
-        env: {
-          ...process.env,
-          PATH: bins.join(':') + ':' + process.env.PATH
-        }
-      })
-
-      p.on('close', (code, signal) => code || signal ? reject(code) : resolve())
-    })
-    prexit(() => promise)
-    return promise
-  }))
 }
 
 function jsonRead(x) {
